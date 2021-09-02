5e07838fa1cb4da4afd62db0357daf4f5462b44a
MarcosReis7 <mreis6357133@gmail.com>
2021-09-02 17:11:10 -0300

    Primeiros exercicios feitos de LP42

---
+++ exLP42_1.py
@@ -0,0 +1,5 @@
+notas = [8, 6, 5, 9]
+soma_notas = sum(notas)
+qt_notas = len(notas)
+media_notas = soma_notas/qt_notas
+print (media_notas)
---
+++ exLP42_3.py
@@ -0,0 +1,5 @@
+v = [1, 2, 3, 4]
+soma_notas = sum(notas)
+qt_notas = len(notas)
+media_notas = soma_notas/qt_notas
+print (media_notas)
